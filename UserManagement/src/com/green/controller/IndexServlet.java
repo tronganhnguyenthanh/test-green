d14ae3e9e54137d609df3bdc903f0d19714373ce
Trong Anh <tronganhnguyen222@gmail.com>
2019-07-24 20:38:33 +0700

    Di chuyển toàn bộ files

---
+++ UserManagement/src/com/green/controller/IndexServlet.java
@@ -0,0 +1,56 @@
+package com.green.controller;
+
+import java.io.IOException;
+import java.util.List;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import com.green.dao.AccountDao;
+import com.green.entity.Account;
+
+/**
+ * Servlet implementation class IndexServlet
+ */
+@WebServlet("")
+public class IndexServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public IndexServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		
+		// lấy danh sách Account từ DB
+		AccountDao accountDao = new AccountDao();
+		List<Account> accountList = accountDao.findAll();
+		
+		// set danh sách account tìm được vào request
+		request.setAttribute("account_list", accountList);
+		
+		// show home page
+		request.getRequestDispatcher("/WEB-INF/home.jsp").forward(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
